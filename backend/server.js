2a3d3bb5721ffe2f7d54f5651e02217e1f14fe53
DESKTOP-UU37Q6O\HP <meenakshidhatura@gmail.com>
2026-05-24 06:35:28 +0530

    updated project

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,3 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import cors from "cors";
-// import { connectDB } from "./config/db.js";
-
-// import authRoutes from "./routes/authRoutes.js";
-// import expenseRoutes from "./routes/expenseRoutes.js";
-
-// dotenv.config();
-// connectDB();
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.use("/api/auth", authRoutes);
-// app.use("/api/expenses", expenseRoutes);
-
-// app.listen(5000, () => console.log("Server running"));
-
 
 import express from "express";
 import dotenv from "dotenv";
